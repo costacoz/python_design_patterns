ab93f0af761268b2f17f074fbd2bcaaa641b9805
Konstantin <buserandi@gmail.com>
2020-11-26 22:17:33 +0200

    Added chain of responsibility

--- behavioral/chain_of_responsibility.py
+++ behavioral/chain_of_responsibility.py
@@ -1,21 +1,120 @@
-# "Single Responsibility" principle says:
-# "Every piece of code must do one, and only one, thing."
+"""""
+Chain of Responsibility.
 
-# Delete this and do my own
+Lets to decouple request sender from request handler, by giving more than one
+object an ability to handle the request.
+Chain the receiving objects and push the request through objects until handled.
 
-class ContentFilter(object):
-    def __init__(self, filters=None):
-        self._filters = list()
-        if filters is not None:
-            self._filters += filters
+Applicability:
+    1. There are many objects that may handle a request. Exact handler isn't
+known a priori. The handler should be selected automatically.
+    2. You want the request to be handled by several objects without specifying
+the receiver explicitly.
+    3. The set of object that can handle the request should be selected automa-
+tically.     
 
-    def filter(self, content):
-        for filter in self._filters:
-            content = filter(content)
-        return content
+Benefits and liabilities:
+    1. Reduced coupling.
+    2. Added flexibility in assigning responsibilities to objects.
+    3. Receipt is not guaranteed.  
+    
+Chain of Responsibility is often applied with Composite.
+There a component's parent can act as its successor.
+"""""
 
-filter = ContentFilter([
-                offensive_filter,
-                ads_filter,
-                porno_video_filter])
-filtered_content = filter.filter(content)
+from abc import ABC, abstractmethod
+from typing import Optional, Tuple, TypeVar
+
+T = TypeVar("T")
+
+
+class Handler(ABC):
+    def __init__(self, successor: Optional[T] = None):
+        self.successor = successor
+
+    def handle(self, request: int) -> None:
+        """
+        Handle request and stop.
+        If can't - call next handler in chain.
+        As an alternative you might even in case of success
+        call the next handler.
+        """
+        res = self.check_range(request)
+        if not res and self.successor:
+            self.successor.handle(request)
+
+    @abstractmethod
+    def check_range(self, request: int) -> Optional[bool]:
+        """Compare passed value to predefined interval"""
+
+
+class ConcreteHandler0(Handler):
+    """Each handler can be different.
+    Be simple and static...
+    """
+
+    @staticmethod
+    def check_range(request: int) -> Optional[bool]:
+        if 0 <= request < 10:
+            print(f"request {request} handled in handler 0")
+            return True
+
+
+class ConcreteHandler1(Handler):
+    """... With it's own internal state"""
+
+    start, end = 10, 20
+
+    def check_range(self, request: int) -> Optional[bool]:
+        if self.start <= request < self.end:
+            print(f"request {request} handled in handler 1")
+            return True
+
+
+class ConcreteHandler2(Handler):
+    """... With helper methods."""
+
+    def check_range(self, request: int) -> Optional[bool]:
+        start, end = self.get_interval_from_db()
+        if start <= request < end:
+            print(f"request {request} handled in handler 2")
+            return True
+
+    @staticmethod
+    def get_interval_from_db() -> Tuple[int, int]:
+        return (20, 30)
+
+
+class FallbackHandler(Handler):
+    @staticmethod
+    def check_range(request: int) -> Optional[bool]:
+        print(f"end of chain, no handler for {request}")
+        return False
+
+
+def main():
+    """
+    >>> h0 = ConcreteHandler0()
+    >>> h1 = ConcreteHandler1()
+    >>> h2 = ConcreteHandler2(FallbackHandler())
+    >>> h0.successor = h1
+    >>> h1.successor = h2
+    >>> requests = [2, 5, 14, 22, 18, 3, 35, 27, 20]
+    >>> for request in requests:
+    ...     h0.handle(request)
+    request 2 handled in handler 0
+    request 5 handled in handler 0
+    request 14 handled in handler 1
+    request 22 handled in handler 2
+    request 18 handled in handler 1
+    request 3 handled in handler 0
+    end of chain, no handler for 35
+    request 27 handled in handler 2
+    request 20 handled in handler 2
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
